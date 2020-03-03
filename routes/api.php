5c6dcb66874157a979547155f74a858835a68e65
ImranSasudin <imransasudin@gmail.com>
2020-03-03 08:54:19 +0800

    Create Read Update Delete

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,15 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+$router->group(['prefix' => ''], function () use ($router) {
+    $router->get('participants',  ['uses' => 'ParticipantController@showAllParticipants']);
+
+    $router->get('participants/{id}', ['uses' => 'ParticipantController@showOneParticipant']);
+
+    $router->post('participants', ['uses' => 'ParticipantController@create']);
+
+    $router->delete('participants/{id}', ['uses' => 'ParticipantController@delete']);
+
+    $router->put('participants/{id}', ['uses' => 'ParticipantController@update']);
+  });
